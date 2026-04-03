ef196f0f63507b7fe66fedfdf5df30ef53ee3f0f
sowho.j <jangseohu112@gmail.com>
2026-04-04 03:36:44 +0900

    first commit

---
+++ myProfile.js
@@ -0,0 +1,8 @@
+const button = document.getElementById("TMI");
+
+button.addEventListener("click", runPractice);
+
+//tmi 정보페이지로 이동
+function runPractice() {
+  window.location.href = "tmi.html";
+}
